50442b969b6e05f18003c8ead8ac7da7ce1fe80b
Hiroaki Nakamura <hnakamur@gmail.com>
2019-10-04 03:06:00 +0900

    Add nginx-svg-module and also update modules

--- download-module-sources.sh
+++ download-module-sources.sh
@@ -83,3 +83,4 @@ download_github_repo -b master hamishforbes/lua-ffi-zlib
 download_github_repo -b master Phrogz/SLAXML
 download_github_repo -b master leev/ngx_http_geoip2_module
 download_github_repo -b master wdaike/ngx_upstream_jdomain
+download_github_repo -b master google/nginx-sxg-module
